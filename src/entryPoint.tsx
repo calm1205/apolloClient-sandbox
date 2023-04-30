0bca49e71cef680db5fd9f5d570092b7f1f3046b
calm1205 <naginani072@gmail.com>
2023-04-30 22:07:11 +0900

    render確認

--- src/entryPoint.tsx
+++ src/entryPoint.tsx
@@ -4,9 +4,9 @@ import { Top } from "./pages/Top/Index";
 import { ApolloProvider } from "./providers/ApolloProvider";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <ApolloProvider>
-      <Top />
-    </ApolloProvider>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <ApolloProvider>
+    <Top />
+  </ApolloProvider>
+  // </React.StrictMode>
 );
