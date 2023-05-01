daa9e95c34dd702b1cc25f5dc11d86dddcca3461
calm1205 <naginani072@gmail.com>
2023-05-01 11:46:04 +0900

    render console

--- src/components/pages/DoubleQuery/components/Company.tsx
+++ src/components/pages/DoubleQuery/components/Company.tsx
@@ -2,10 +2,13 @@ type Company = {
   name?: string | null;
   summary?: string | null;
 };
-export const Company: React.FC<Company> = ({ name, summary }) => (
-  <>
-    <h2>Company</h2>
-    <b>{name}</b>
-    <p>{summary}</p>
-  </>
-);
+export const Company: React.FC<Company> = ({ name, summary }) => {
+  console.log("render company");
+  return (
+    <>
+      <h2>Company</h2>
+      <b>{name}</b>
+      <p>{summary}</p>
+    </>
+  );
+};
--- src/components/pages/DoubleQuery/components/Histories.tsx
+++ src/components/pages/DoubleQuery/components/Histories.tsx
@@ -11,14 +11,17 @@ type Histories = {
     | null;
 };
 
-export const Histories: React.FC<Histories> = ({ histories }) => (
-  <>
-    <h2>History</h2>
-    {histories?.map((history) => (
-      <div key={history?.title}>
-        <b>{history?.title}</b>
-        <p>{history?.details}</p>
-      </div>
-    ))}
-  </>
-);
+export const Histories: React.FC<Histories> = ({ histories }) => {
+  console.log("render history");
+  return (
+    <>
+      <h2>History</h2>
+      {histories?.map((history) => (
+        <div key={history?.title}>
+          <b>{history?.title}</b>
+          <p>{history?.details}</p>
+        </div>
+      ))}
+    </>
+  );
+};
